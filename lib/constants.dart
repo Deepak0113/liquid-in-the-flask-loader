afe8eeb4fc44a06bba9482b66b6877474d86772b
Deepak0113 <mdeecreations@gmail.com>
2021-03-02 22:59:40 +0530

    commit

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color KTopColor = Colors.green; //Color(0xffffa27d);
+const Color KBottomColor = Color(0xffc83831);
+const Color KContourColor = Color(0xffff8670);
+const int KWaveLength = 230;
+const Size kSize = Size(200, 200);
